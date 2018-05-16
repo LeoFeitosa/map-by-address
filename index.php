a1f05dbfec0f9cf098003a707cef53dd02f78fca
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-16 10:19:11 -0300

    File with the function used

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+function getFrameGoogleMaps($endereco)
+{
+  $sslConfig=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+  );
+
+  $geolocalizacao = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.str_replace(' ', '+', $endereco).'&key=AIzaSyAVPfZsQ5IkL89k2mRdEQr2iMznSZdFEcM', false, stream_context_create($sslConfig));
+
+  $saidaGeo = json_decode($geolocalizacao);
+
+  $latitude = $saidaGeo->results[0]->geometry->location->lat;
+  $longitude = $saidaGeo->results[0]->geometry->location->lng;
+
+  return '<iframe src=https://maps.google.com/maps?q='.$latitude.','.$longitude.'&hl=pt-BR&lr=lang_pt&z=16&amp&output=embed width=600 height=450 frameborder=0 style="border:0" allowfullscreen></iframe>';
+}
+
+$endereco = 'cptm itaquaquecetuba';
+
+echo getFrameGoogleMaps($endereco);
